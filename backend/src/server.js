d1d4a3f90839e80d7b90fa93b2d48b2e0dc45ad2
Carlos Lima (kvasir) <carloskvasir@gmail.com>
2019-10-09 09:50:23 -0300

    pegando dados de todas as formas

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,8 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.put('/users/:id', (req, res) => {
-  return res.json({ id: req.params.id });
+// GET, POST, PUT, DELETE
+
+// req.query = Acessar query params (para filtros)
+// req.params = Acessar route params (para edição, delete)
+// req.body = Acessar corpo da requisição (para criação, edição)
+
+app.use(express.json());
+
+app.post('/users', (req, res) => {
+  return res.json(req.body);
 });
 
 app.listen(3333);
