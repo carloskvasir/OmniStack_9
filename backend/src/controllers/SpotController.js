2fceaa8e083f25ac402ca4e1d30c52540547105b
Carlos Lima (kvasir) <carloskvasir@gmail.com>
2019-10-09 15:06:26 -0300

    save in db spot

--- backend/src/controllers/SpotController.js
+++ backend/src/controllers/SpotController.js
@@ -2,9 +2,24 @@ const Spot = require('../models/Spot');
 
 module.exports = {
   async store(req, res) {
-    console.log(req.body);
-    console.log(req.file);
+    const { filename } = req.file;
+    const { company, techs, price } = req.body;
+    const { user_id } = req.headers;
 
-    return res.json({ ok: true });
+    const user = await user.findByid(user_id);
+
+    if (!user) {
+      return res.status(400).json({ error: 'User does not exists.' })
+    }
+
+    const spot = await Spot.create({
+      user: user_id,
+      thumbnail: filename,
+      company,
+      techs: techs.split(',').map(tech => tech.trim()),
+      price
+    });
+
+    return res.json(spot);
   }
 }
