bbba26e95325be1362baf3ebb4af87a749f537a6
Carlos Lima (kvasir) <carloskvasir@gmail.com>
2019-10-09 09:29:51 -0300

    testando post

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.post('/users', (req, res) => {
   return res.json({ message: 'Hello World' });
 });
 
